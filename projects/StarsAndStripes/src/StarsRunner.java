53c799ff47020d95b00319676c2f3678fe3fb55c
PreedomCL <54563759+PreedomCL@users.noreply.github.com>
2022-08-22 20:57:11 -0600

    Recreate StarsAndStripes project

--- projects/StarsAndStripes/src/StarsRunner.java
+++ projects/StarsAndStripes/src/StarsRunner.java
@@ -4,7 +4,7 @@
 
 public class StarsRunner
 {
-   public static void main(String args[])
+   public static void main(String[] args)
    {
 	StarsAndStripes stars = new StarsAndStripes();
 	stars.printTwentyStars();
