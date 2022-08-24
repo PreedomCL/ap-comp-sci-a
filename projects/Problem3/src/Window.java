31ec2f0d0f7a4e77365e3186545336a586c85200
preedomcl <54563759+PreedomCL@users.noreply.github.com>
2022-08-24 12:19:31 -0600

    Complete Problem 3

---
+++ projects/Problem3/src/Window.java
@@ -0,0 +1,13 @@
+public class Window {
+    public static void main(String[] args) {
+        System.out.println("#########\n" +
+                "#   #   #\n" +
+                "#########\n" +
+                "#   #   #\n" +
+                "#########\n" +
+                "#   #   #\n" +
+                "#########\n" +
+                "#   #   #\n" +
+                "#########");
+    }
+}
