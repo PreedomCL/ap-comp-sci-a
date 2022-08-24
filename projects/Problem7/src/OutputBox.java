b29bde061b7d8d3b39af0148ea2093e061175fdc
preedomcl <54563759+PreedomCL@users.noreply.github.com>
2022-08-24 12:50:20 -0600

    Complete Problem 7

---
+++ projects/Problem7/src/OutputBox.java
@@ -0,0 +1,15 @@
+public class OutputBox {
+    public static void main(String[] args) {
+        System.out.println("AAAAAAAAAAA\n" +
+                "ABBBBBBBBBA\n" +
+                "ACCCCCCCCCA\n" +
+                "ADDDDDDDDDA\n" +
+                "AEEEEEEEEEA\n" +
+                "AFFFFFFFFFA\n" +
+                "AEEEEEEEEEA\n" +
+                "ADDDDDDDDDA\n" +
+                "ACCCCCCCCCA\n" +
+                "ABBBBBBBBBA\n" +
+                "AAAAAAAAAAA");
+    }
+}
