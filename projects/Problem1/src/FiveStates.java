539403f2e99804b27f47ada78748f7d9dc81991e
preedomcl <54563759+PreedomCL@users.noreply.github.com>
2022-08-24 12:09:56 -0600

    Complete Problem 1

---
+++ projects/Problem1/src/FiveStates.java
@@ -0,0 +1,9 @@
+public class FiveStates {
+    public static void main(String[] args) {
+        System.out.println("ALASKA");
+        System.out.println("TEXAS");
+        System.out.println("NEW MEXICO");
+        System.out.println("OKLAHOMA");
+        System.out.println("FLORIDA");
+    }
+}
