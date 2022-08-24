81bf090a5fe50c28ac12af3d5be759bc91cebf3d
preedomcl <54563759+PreedomCL@users.noreply.github.com>
2022-08-24 11:53:29 -0600

    Complete Unit 5 Lab 3

--- projects/Unit5Lab3/src/Launcher.java
+++ projects/Unit5Lab3/src/Launcher.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Launcher {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        QuadraticEquation quad = new QuadraticEquation();
+        for(int i = 0; i < 3; i++) {
+            System.out.print("a: ");
+            double a = scanner.nextDouble();
+            System.out.print("b: ");
+            double b = scanner.nextDouble();
+            System.out.print("c: ");
+            double c = scanner.nextDouble();
 
+            double[] answers = quad.calc(a, b, c);
+            if(answers != null) {
+                System.out.println("Answer: " + answers[0] + " or " + answers[1]);
+            }
+        }
     }
 }
--- projects/Unit5Lab3/src/QuadraticEquation.java
+++ projects/Unit5Lab3/src/QuadraticEquation.java
@@ -4,12 +4,12 @@ public class QuadraticEquation {
     public double[] calc(double a, double b, double c) {
         double[] answers = new double[2];
 
-        double d = (b * b) - 4 * a * c;
+        double d = (b * b) - (4 * a * c);
         if(d < 0) {
             return null;
         }
-        answers[0] = ((-b + Math.sqrt(d))/(2*a));
-        answers[1] = ((-b - Math.sqrt(d))/(2*a));
+        answers[0] = ((-1 * b) + Math.sqrt(d))/(2D*a);
+        answers[1] = ((-1 * b) - Math.sqrt(d))/(2D*a);
         return answers;
     }
 }
