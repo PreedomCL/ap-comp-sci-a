d4dd0ccedc7b415564df9172f57a3e6f3742fa8d
preedomcl <54563759+PreedomCL@users.noreply.github.com>
2022-09-07 12:52:23 -0600

    Complete problems 25 and 26

---
+++ singleFileProjects/Problem26.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Problem26 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner file = new Scanner(new File("data.dat"));
+        int j = file.nextInt();
+        for (int i = 0; i < j; i++) {
+            int x = file.nextInt();
+            int y = file.nextInt();
+
+            System.out.println(Integer.toBinaryString(x));
+            System.out.println(Integer.toBinaryString(y));
+            System.out.println(Integer.toBinaryString(x|y));
+            System.out.println(x|y);
+        }
+    }
+}
