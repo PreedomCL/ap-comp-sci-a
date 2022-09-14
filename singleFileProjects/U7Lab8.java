1bc2bce7ce48513e31dc2de121ef2a3196ee1765
preedomcl <54563759+PreedomCL@users.noreply.github.com>
2022-09-14 11:46:14 -0600

    Complete Unit 7 Lab 8

--- singleFileProjects/U7Lab8.java
+++ singleFileProjects/U7Lab8.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class U7Lab8 {
@@ -12,6 +13,12 @@ public class U7Lab8 {
     }
 
     public static String checkString(String string) {
-        if(string.charAt(0) > )
+        char f = string.toLowerCase(Locale.ROOT).charAt(0);
+        if(f == 'a' || f == 'e' || f == 'i' || f == 'o' || f == 'u')
+            return "yes";
+        f = string.toLowerCase(Locale.ROOT).charAt(string.length()-1);
+        if(f == 'a' || f == 'e' || f == 'i' || f == 'o' || f == 'u')
+            return "yes";
+        return "no";
     }
 }
