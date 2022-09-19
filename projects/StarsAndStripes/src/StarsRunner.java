66476592c86dc5fbfc2f6a14d24ff21609848346
preedomcl <54563759+PreedomCL@users.noreply.github.com>
2022-09-19 12:25:33 -0600

    Start Unit 8 Lab 1 and LabCreator util class

--- projects/StarsAndStripes/src/StarsRunner.java
+++ projects/StarsAndStripes/src/StarsRunner.java
@@ -2,9 +2,16 @@
 //www.apluscompsci.com
 //Name -
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class StarsRunner {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		StarsAndStripes stars = new StarsAndStripes();
+
+		Scanner scanner = new Scanner(new File("src/readme.dat"));
+		System.out.println(scanner.nextLine());
 		stars.printTwentyStars();
 		for(int i = 0; i < 9; i++) {
 			stars.printTwentyDashes();
