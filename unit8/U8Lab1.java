5dcff520715d229235aab19cf20c9dda1c146ae0
PreedomCL <54563759+PreedomCL@users.noreply.github.com>
2022-09-19 18:49:54 -0600

    Complete Unit 8 Lab 1

--- unit8/U8Lab1.java
+++ unit8/U8Lab1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class U8Lab1 {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File("data1.dat"));
+        Scanner scanner = new Scanner(new File("U8Lab1Data.dat"));
 
         while(scanner.hasNextLine()) {
             System.out.println(addSNN(scanner.nextLine()));
@@ -16,8 +16,8 @@ public class U8Lab1 {
         if(split.length != 3)
             return "SS# " + SSN + " has a total of " + -1;
         int sum = 0;
-        for(int i = 0; i < split.length; i++) {
-            sum += Integer.parseInt(split[i]);
+        for(String s: split) {
+            sum += Integer.parseInt(s);
         }
         return "SS# " + SSN + " has a total of " + sum;
     }
